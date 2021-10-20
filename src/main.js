a99e9c84a139d7edd9a9bd902f9fb0d7faef3e45
Krzysztof Szafraniak <k.szafraniak1@wp.pl>
2021-10-21 00:42:17 +0200

    `[0.3.0]` Added: BMP Decoder/Encoder & Blob downloader

--- src/main.js
+++ src/main.js
@@ -1,2 +1,46 @@
-// Development Entry Point
-console.log("Hey!");
+
+
+// // examples:
+// const fileInput = document.querySelector('#formFileSm');
+//
+// // 3D array : 3 rows x 2 cols x 3 channels
+// const pixels3dData = [
+//     [[0, 0, 0], [255, 255, 255]],
+//     [[255, 255, 255], [0, 0, 0]],
+//     [[0, 0, 0], [255, 255, 255]],
+// ]
+//
+// encodeBMPFrom3dData(pixels3dData)
+//     .then(blob => decodeBMP(blob))
+//     .then(dec => console.log(dec))
+//
+// // Flat array : 3 rows x 2 cols x 3 channels
+// const flatPixelData = [
+//     0, 0, 0, 255, 255, 255,
+//     255, 255, 255, 0, 0, 0,
+//     0, 0, 0, 255, 255, 255,
+// ]
+//
+// encodeBMP(flatPixelData, 2, 3)
+//     .then(blob => decodeBMP(blob))
+//     .then(dec => console.log(dec))
+
+//
+// // Download blob:
+// encodeBMPFrom3dData(pixels3dData)
+//     .then(blob => downloadBlob(blob, "hello-world.bmp"));
+//
+//
+// // modify image:
+// fileInput.addEventListener('input', async e => {
+//     const file = e.target.files[0];
+//     const decodedFile = await decodeBMP(file);
+//     const pixel3dArray = decodedFile.pixels3D;
+//     console.log(decodedFile)
+//
+//     // Make image upside down
+//     const upsideDownImg = pixel3dArray.reverse();
+//
+//     const blob = await encodeBMPFrom3dData(upsideDownImg);
+//     downloadBlob(blob, 'upside-down-' + file.name);
+// });
